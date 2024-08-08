c79adb55eb78c8662d05ccf5e49f4205fd474b84
Beckham <vovanbeckham@yandex.ru>
2024-08-08 20:55:46 +0300

    filter service provided 1 week

--- backend/core/manicure/views.py
+++ backend/core/manicure/views.py
@@ -8,7 +8,6 @@ from manicure.models import ProvidedService, Service
 
 
 def index(request):
-    menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
-    today = datetime.datetime.now()
-    services = ProvidedService.objects.all()
+    last_day_week = datetime.datetime.now() - datetime.timedelta(days=7)
+    services = ProvidedService.objects.filter(date__gte=last_day_week).order_by('date')
     return render(request, "index.html", locals())
